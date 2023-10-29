8f6150db951077d4c2217575ebd0fadcdff24f6d
Pavel <pavel2002@ryabov.co>
2023-10-29 11:54:07 -0700

    cleanup

--- backend/convex/actions.ts
+++ backend/convex/actions.ts
@@ -4,9 +4,7 @@ import { httpAction } from "./_generated/server";
 export const getPosts = httpAction(async (ctx, request) => {
     const posts = await ctx.runMutation(api.posts.list);
 
-    const resp2 = new Response(JSON.stringify(posts), {
+    return new Response(JSON.stringify(posts), {
         status: 200,
     });
-
-    return resp2;
 });
